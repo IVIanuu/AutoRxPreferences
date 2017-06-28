9daea3e23cb8c39518750c92c41eeb38b190e94f
IVIanuu <IVIanuu@gmail.com>
2017-06-28 03:52:30 +0200

    Fixed build

--- sample/src/main/java/com/ivianuu/autorxpreferences/sample/MainActivity.java
+++ sample/src/main/java/com/ivianuu/autorxpreferences/sample/MainActivity.java
@@ -3,6 +3,8 @@ package com.ivianuu.autorxpreferences.sample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import io.reactivex.functions.Consumer;
+
 public class MainActivity extends AppCompatActivity {
 
     private SamplePreferences_ samplePreferences;
