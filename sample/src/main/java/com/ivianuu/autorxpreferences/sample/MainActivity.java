059090e51e7df592f59e3eac3989b29d5b702f80
IVIanuu <IVIanuu@gmail.com>
2017-07-23 18:57:50 +0200

    Added support for lists or other parameterized types

--- sample/src/main/java/com/ivianuu/autorxpreferences/sample/MainActivity.java
+++ sample/src/main/java/com/ivianuu/autorxpreferences/sample/MainActivity.java
@@ -20,5 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         samplePreferences = SamplePreferences_.create(this);
+        samplePreferences.getAccessToken().get();
     }
 }
